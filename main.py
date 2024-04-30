6c145cfde285cf5c8a2906bd7c269d6b865a87d0
MartinoVespa <m.vespa@alumno.um.edu.ar>
2024-04-30 14:01:41 -0300

    archivo main

---
+++ main.py
@@ -0,0 +1,15 @@
+from persona import persona
+
+if __name__ == '__main__':
+
+    persona = persona()
+
+    print(persona.__nombre__)
+
+    persona = persona()
+
+    print(persona.__apellido__)
+
+    persona = persona()
+
+    print(persona.__dni__)
